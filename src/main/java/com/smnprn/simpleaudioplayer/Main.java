ea95a7ee896b46f2e99c5599dac7d8fbe399dace
smnprn <simoneperna8@gmail.com>
2024-03-07 23:47:52 +0100

    ref: separated stage and scene setting

--- src/main/java/com/smnprn/simpleaudioplayer/Main.java
+++ src/main/java/com/smnprn/simpleaudioplayer/Main.java
@@ -19,6 +19,14 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 650, 850);
+
+        setScene(scene, stage);
+        setStage(stage, scene);
+
+        stage.show();
+    }
+
+    public void setScene(Scene scene, Stage stage) {
         scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
 
         scene.setOnMousePressed(event -> {
@@ -30,11 +38,12 @@ public class Main extends Application {
             stage.setX(event.getScreenX() - xOffset);
             stage.setY(event.getScreenY() - yOffset);
         });
+    }
 
+    public void setStage(Stage stage, Scene scene) {
         stage.setTitle("Simple Audio Player");
         stage.initStyle(StageStyle.UNDECORATED);
         stage.setScene(scene);
-        stage.show();
     }
 
     public static void main(String[] args) {
